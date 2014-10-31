77d43a61120516eda8fd03ad79c3e93de22e5848
logangilmour <logan@scopear.com>
2014-10-31 03:53:27 -0600

    Add first test

--- Assets/Generate.cs
+++ Assets/Generate.cs
@@ -1,11 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using QuickMesh;
 
 public class Generate : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Material mat = (Material)Resources.Load("Brown", typeof(Material));
+
+
+		GameObject plane = new GameObject("GenTree");
+		plane.transform.position = new Vector3(0,0,0);
+
+
+		MeshFilter meshFilter = (MeshFilter)plane.AddComponent(typeof(MeshFilter));
+
+		Selection s = new Selection ();
+		s.Circle (5);
+		meshFilter.mesh  = s.Finish ();
+
+		MeshRenderer renderer = plane.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+		renderer.sharedMaterial = mat;
+		
 	}
 	
 	// Update is called once per frame
